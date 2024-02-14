72012ee9b85fa4fb9c9eeeeb27f75c982bd2359c
Piotr S <piotr1989@interia.eu>
2024-02-14 22:46:17 +0100

    test#3

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 public class ApiTest {
 
